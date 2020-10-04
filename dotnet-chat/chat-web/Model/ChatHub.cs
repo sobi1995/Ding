02ea0388b03977d0f8bf180dcfaccc85c195d22f
Sobi <sobhan.plus.plus@gmail.com>
2020-10-04 12:48:51 +0330

    Implement Group Chat :  )

--- dotnet-chat/chat-web/Model/ChatHub.cs
+++ dotnet-chat/chat-web/Model/ChatHub.cs
@@ -10,37 +10,17 @@ namespace chatweb.Model
     public class ChatHub : Hub
     {
         private static readonly ConcurrentDictionary<string, UserSocket> Users = new ConcurrentDictionary<string, UserSocket>();
-        private static readonly ConcurrentDictionary<string, UserSocket> Room = new ConcurrentDictionary<string, UserSocket>();
-        private static object _lock = new object();
+
+        public byte MaxMemberOfGroup { get; set; }
+
 
         //public async Task SendMessage(string message)
         //{
         //    await Clients.All.SendAsync("ReceiveMessage", message);
         //}
-        public void Send(SendMessage message)
+        public ChatHub()
         {
-            //message.read = 0;
-            //string usernamecontact = _user.Where(x => x.Id == message.userr).Select(x => x.Username).FirstOrDefault();
-
-            //var conationsids = GetConnection(usernamecontact);
-            //var my = GetConnection(Context.User.Identity.Name);
-            //message.id = _chat.SaveMessage(message);
-            //if (conationsids != null)
-
-            //    foreach (var conationsid in conationsids)
-            //    {
-
-            //        Clients.Client(conationsid).SendAsync("Send", message);
-            //    }
-            ////!!این قسمت باید اصلح شود
-
-            //if (my != null)
-
-            //    foreach (var conationsid in my)
-            //    {
-
-            //        Clients.Client(conationsid).SendAsync("Send", message);
-            //    }
+            MaxMemberOfGroup = 2;
         }
 
         public void SendSpecialUserAllConations(string[] conationsids, string message)
@@ -53,13 +33,34 @@ namespace chatweb.Model
         }
         public void SendSpecialUser(string conationsid, Message message)
         {
+
+          
                 Clients.Client(conationsid).SendAsync("ReceiveMessage", message);  
+        }
+        public Task SedndMessageGroupExceptCurentUser( string groupname,  string message)
+        {
+            return Clients.GroupExcept(groupname, Context.ConnectionId).SendAsync("ReceiveMessage", new Message() { GroupName});
+        }
+        public Task SedndMessageGroup( string groupname, Message message)
+        {
+            return Clients.Group(groupname).SendAsync("ReceiveMessage", message);
+        }
+        public  Task JoinGroup(string conationsid, string group)
+        {
+            return Groups.AddToGroupAsync(conationsid, group);
         }
         public void FoundUserAndCreateChat()
         {
-          if (Users.Keys.Count<=1)
+            var user = Users.Where(x => x.Value.IsChating == false && x.Value.ConnectionIds.Any(x=> x!= Context.ConnectionId)).FirstOrDefault();
+
+         
+            if (user.Key!=null)
             {
-                SendSpecialUser(Context.ConnectionId, new Message()  {Msg=null,Status=Status.Waiting});
+                var groupName = Context.ConnectionId + user.Value.ConnectionIds.FirstOrDefault();
+                JoinGroup(Context.ConnectionId, groupName);
+                JoinGroup(user.Value.ConnectionIds.FirstOrDefault(), groupName);
+
+                SedndMessageGroup(groupName, new Message() { Msg="The User Is Found",Status=Status.FoundUser,GroupName= groupName });
             }
 
         }
@@ -73,7 +74,8 @@ namespace chatweb.Model
                 _ => new UserSocket
                 {
                     Name = connectionId,
-                    ConnectionIds = new HashSet<string>()
+                    ConnectionIds = new HashSet<string>(),
+                    IsChating=false
                 });
             lock (user.ConnectionIds)
             {
@@ -115,7 +117,7 @@ namespace chatweb.Model
     {
 
         public string Name { get; set; }
-        public Status Status { get; set; }
+        public bool IsChating { get; set; }
         public HashSet<string> ConnectionIds { get; set; }
     }
 
@@ -131,8 +133,11 @@ namespace chatweb.Model
 
         Chating = 1,
         Waiting = 0,
-        NoneOnline=3
+        NoneOnline=3,
+        FoundUser=4
     }
+
+
     public class UserConnectionSignalR
     {
         public int Id { set; get; }
@@ -154,15 +159,18 @@ namespace chatweb.Model
         public string Msg { get; set; }
         public Status Status { get; set; }
         public DateTime Date { get; set; }
+        public string GroupName { get; set; }
         public Message()
         {
             Date = DateTime.Now;
         }
     }
 
-    public class Room {
-        public string UserId1 { get; set; }
-        public string UserId2 { get; set; }
+    public class MessageGroup {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public Status Status { get; set; }
+
 
     }
 
