74cf28877bd1e10dffd058f906ec70b9515f60b1
Sobi <sobhan.plus.plus@gmail.com>
2020-10-04 14:02:49 +0330

    first chat

--- src/app/chat-room/chat-room.component.ts
+++ src/app/chat-room/chat-room.component.ts
@@ -9,60 +9,76 @@ import { timer } from 'rxjs';
   styleUrls: ['./chat-room.component.css']
 })
 export class ChatRoomComponent implements OnInit {
+  // tslint:disable-next-line:variable-name
   private _hubConnection: HubConnection;
   message = '';
-  messages: string[] = [];
+  messages:any=[];
 
-  UserName='';
-  Conected=false;
-  ShowLoader=false
+  GroupName: '';
+  UserName = '';
+  Conected = true;
+  ShowLoader = true;
 
   title = ' SignarlR';
 
 
+  // tslint:disable-next-line:typedef
   ngOnInit() {
-
+    this.StartSocket();
   }
 
   public sendMessage(): void {
-    const data = `SendMessage: ${this.message}`;
+    const data = `Me : ${this.message}`;
 
-    this._hubConnection.invoke('SendMessage', data);
-    this.messages.push(data);
+    this._hubConnection.invoke('SedndMessageGroupExceptCurentUser', this.GroupName, this.message);
+    this.messages.push({message : this.message, type : true});
+    console.log(this.messages);
+    this.message='';
   }
-  scroll(){
-    let myDiv = document.querySelector("#scroll")
-    myDiv.scrollIntoView()
-  }
-
-
-  Login():void{
-    const data = this.UserName
-      this.Conected=true
-      this.ShowLoader=true
+  // tslint:disable-next-line:typedef
+  scroll() {
+    const myDiv = document.querySelector("#scroll")
+    myDiv.scrollIntoView();
   }
 
 
-  StartSocket(){
+  Login(): void {
+    const data = this.UserName;
+    this.Conected = true;
+    this.ShowLoader = true;
+  }
+
+
+  // tslint:disable-next-line:typedef
+  StartSocket() {
 
     this._hubConnection = new HubConnectionBuilder().withUrl('https://localhost:44300/chathub')
-                            .build();
+      .build();
 
     this._hubConnection.on('ReceiveMessage', (data: any) => {
 
+      console.log(data);
 
-       this.ShowLoader=data.status==0 ? true:false;
+      if (data.status === 4) {
+        this.ShowLoader = false;
+        this.GroupName = data.groupName;
+          }
+
+     else if (data.status === 1) {
         const received = `Received: ${data}`;
-        this.messages.push(received);
+        this.messages.push({message : data.message, type : false});
+        console.log(this.messages);
+
+      }
     });
 
     this._hubConnection.start()
-        .then(() => {
-            console.log('Hub connection started');
-        })
-        .catch(err => {
-            console.log('Error while establishing connection');
-        });
+      .then(() => {
+        console.log('Hub connection started');
+      })
+      .catch(err => {
+        console.log('Error while establishing connection');
+      });
 
   }
 }
