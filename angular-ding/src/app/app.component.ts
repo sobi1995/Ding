8e03dd330e2500f58acf4712b0a9eda7cc340c47
Sobi <sobhan.plus.plus@gmail.com>
2020-10-22 20:30:18 +0330

    ismobile in  angualr

--- angular-ding/src/app/app.component.ts
+++ angular-ding/src/app/app.component.ts
@@ -1,9 +1,36 @@
 
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent  implements OnInit {
+ 
+constructor( 
+  public swUpdate: SwUpdate,
+  private deviceService: DeviceDetectorService,
+  private router:Router){
+
+  if(this.swUpdate.isEnabled)
+  {
+    this.swUpdate.available.subscribe(()=> {
+        alert("برنامه با قابلیت های جدید ابدیت شد :  )")
+        window.location.reload();
+    })
+  }
+
+}
+  ngOnInit(): void {
+  if(!this.deviceService.isMobile() ){
+this.router.navigate(["/WebPlatform"])
+  }
+  else{
+    this.router.navigate(["/chat-room"])
+  }
+  }
+
+}
