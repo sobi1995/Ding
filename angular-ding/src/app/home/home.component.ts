ec1a5d257b69f30d830aec04e972bf17ad927c2f
saman azadi <samanazadi1996@gmail.com>
2020-10-25 22:14:57 +0330

    add slider to home page

--- angular-ding/src/app/home/home.component.ts
+++ angular-ding/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {
+    setInterval(() => {
+      if (this.timerEnable) {
+        
+      this.slideIndex+=this.ind;
+      var x = document.getElementsByClassName("mySlides");
+      if (this.slideIndex > x.length) {this.slideIndex = 1}
+      if (this.slideIndex < 1) {this.slideIndex = x.length}
+    }}, 3000);   
+  }
   ngOnInit(): void {
   }
-
+  slideIndex=1;
+  ind=1
+timerEnable=true
+  slider(n) {
+    this.timerEnable=false
+ this.ind=n;
+    this.slideIndex+=n;
+    var x = document.getElementsByClassName("mySlides");
+    if (this.slideIndex > x.length) {this.slideIndex = 1}
+    if (this.slideIndex < 1) {this.slideIndex = x.length}
+    setTimeout(() => {
+      this.timerEnable=true;
+    }, 3000);    
+  }
+  
 }
